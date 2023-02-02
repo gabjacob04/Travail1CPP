74ccd514c517592e7820e207b077bd0d8b600b52
gabjacob04 <jedigabjacob@icloud.com>
2023-02-02 12:19:42 -0500

    Étapes 1 fini

--- SuperDir/SuperDir/SuperDir.cpp
+++ SuperDir/SuperDir/SuperDir.cpp
@@ -1,11 +1,12 @@
 // SuperDir.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
 
+using namespace std;
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
